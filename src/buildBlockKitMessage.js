d488f60e3caa5dd38285dc056d506004c12fc156
Paulo Godinho <paulo@godinhosilva.com.br>
2022-11-27 19:50:54 -0300

    Even more testing

--- src/buildBlockKitMessage.js
+++ src/buildBlockKitMessage.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const outputFile = process.env.GITHUB_OUTPUT
 
-fs.appendFile(outputFile, '\nAAAA=BBBB\n', function (err) {
+fs.appendFile(outputFile, 'AAAA=BBBB\n', function (err) {
   if (err) throw err;
   console.log('Added to output');
 });
