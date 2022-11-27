0491ada625c5d9da8d43e852d47ce8db54db643b
Paulo Godinho <paulo@godinhosilva.com.br>
2022-11-27 19:37:51 -0300

    even more testing

--- src/buildBlockKitMessage.js
+++ src/buildBlockKitMessage.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
+fs = require('fs')
+
 
 const outputFile = process.env.GITHUB_OUTPUT
+
+fs.readFile(outputFile, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+});
+  
+
 
 fs.appendFile(outputFile, 'AAAA=BBBB', function (err) {
   if (err) throw err;
   console.log('Added to output');
+});
+
+fs.readFile(outputFile, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
 });
 
 console.log("Yep");
