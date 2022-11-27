259bbee534441f91137e7d7a68ab5e7fe9cbaaf1
Paulo Godinho <paulo@godinhosilva.com.br>
2022-11-27 18:27:41 -0300

    More tests!

---
+++ src/buildBlockKitMessage.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+// fs.appendFile('message.txt', 'data to append', function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+console.log(process.env);
