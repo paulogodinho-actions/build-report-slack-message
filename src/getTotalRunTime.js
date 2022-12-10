3c1ef71328f3269c4ab1eb8e0ee9efa6b7346b50
Paulo Godinho <paulo@godinhosilva.com.br>
2022-12-10 00:38:04 -0300

    More testing

--- src/getTotalRunTime.js
+++ src/getTotalRunTime.js
@@ -1,11 +1,7 @@
-const owner = context.payload.repository.owner.login
-const repo = context.payload.repository.name
-const run_id = context.runId
-const response = await github.request(`GET /repos/${owner}/${repo}/actions/runs/${run_id}`, {
-    owner: `${owner}`,
-    repo: `${repo}`,
-    run_id: `${repo}`
-  })
+console.log(process.argv)
+
+return
+
 console.log(`Run Started at: ${response.data.run_started_at}`)
 const started = Date.parse(response.data.run_started_at);
 const utcNow = new Date().getTime();
